52bec003bda78a7a12993b72279b59c738ca8174
OrMizrahi <51017125+OrMizrahi@users.noreply.github.com>
2020-03-22 21:54:05 +0200

    test

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import '/src/styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
