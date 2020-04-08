e01e8b2462d01ec60501f000f6c60553b320109d
or mizrahi <ormizrahi777@gmail.com>
2020-04-08 15:18:01 +0300

    fixed a bug

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -11,10 +11,10 @@ export const ExpenseList = (props) => {
 				<div className='show-for-desktop'>Expense</div>
 				<div className='show-for-desktop'>Amount</div>
 			</div>
-			{props.hiddenExpensesNumber > 0 && (
-				<p>Total Hidden Expenses: {props.hiddenExpensesNumber}</p>
-			)}
 			<div className='list-body'>
+				{props.hiddenExpensesNumber > 0 && (
+					<p>Total Hidden Expenses: {props.hiddenExpensesNumber}</p>
+				)}
 				{props.expenses.length === 0 ? (
 					<div className='list-item  list-item--message'>
 						<span>No Expenses To show</span>
